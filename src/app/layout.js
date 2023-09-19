10386988ab7c438285c968b546c289da93c97d62
JRH89 <thegemlins@gmail.com>
2023-09-18 19:40:19 -0700

    more metas

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,9 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta property="og:title" content="${metadata.title}" />
-        <meta property="og:description" content="${metadata.description}" />
+        <meta property="og:title" content="Time Tracker" />
+        <meta property="og:description" content="A tool for allowing freelancers to track how much time they have spent working on a project, client expense tracker." />
         <meta property="og:image" content="https://time-tracker-plus.vercel.app/preview.png" />
+        <meta property="og:url" content="https://time-tracker-plus.vercel.app" />
       </head>
       <body className={inter.className}>
         <AuthProvider>
