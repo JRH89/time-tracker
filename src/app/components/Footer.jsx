e2305fa8ca8d4f1bb83c7506314cafa0a0893ced
Jared Hooker <68973848+JRH89@users.noreply.github.com>
2023-10-12 22:20:21 -0700

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 	return (
 		<footer className="bg-gray-800 text-white py-2 text-center">
 			<div className="container mx-auto">
-				<p>Powered By: <a href="https://hooker-hill-studios.vercel.app" className="text-blue-400 hover:underline">Hooker Hill Studios</a></p>
+				<p>Powered By: <a href="https://www.hookerhillstudios.com" className="text-blue-400 hover:underline">Hooker Hill Studios</a></p>
 			</div>
 		</footer>
 	)
